5a676251e5f6eb9adb4a59c4d0e4ffe2e4713b28
Regina Gimazova <reginagimazova98@gmail.com>
2019-06-02 14:21:34 +0300

    build scripts

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
-import { Route, BrowserRouter, Switch } from "react-router-dom";
+import { Route, BrowserRouter, Switch, HashRouter } from "react-router-dom";
 import MainPage from "./pages/mainPage";
 
 class App extends Component {
   render() {
     return (
-        <BrowserRouter>
+        <HashRouter>
           <Switch>
             <Route path="/" component={MainPage} exact />
           </Switch>
-        </BrowserRouter>
+        </HashRouter>
     );
   }
 }
