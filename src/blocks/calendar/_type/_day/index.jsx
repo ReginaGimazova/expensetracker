28f0cb48b4d0c331abe1301431a0244586f0dc88
Regina Gimazova <reginagimazova98@gmail.com>
2019-06-22 21:36:42 +0300

    try to fix routing

--- src/blocks/calendar/_type/_day/index.jsx
+++ src/blocks/calendar/_type/_day/index.jsx
@@ -111,7 +111,7 @@ class CalendarDay extends Component {
   render() {
     const { currentDay } = this.state;
     return (
-      <div className="container">
+      <div className="container_calendar">
         <CalendarNav date={this.dateToShow()} prevClick={this.prevDay} nextClick={this.nextDay} />
         <div className="container_flex">
           <table className="calendar_type_day">
