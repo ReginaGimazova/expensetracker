e363c07c8de5cbccafe877d696213887d7b4c6d7
Regina-Gimazova <regina.gimazova@flatstack.com>
2019-07-22 13:47:17 +0300

    try to fix eslint error in deployed app

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import ExpensePage from './pages/expensesPage';
 import RegistrationPage from './pages/registrationPage';
 
 const App = () => (
-  <BrowserRouter basename="https://reginagimazova.github.io/expenseTracker">
+  <BrowserRouter>
     <Switch>
       <Route path="/auth" component={AuthPage} exact />
       <Route path="/registration" component={RegistrationPage} exact />
