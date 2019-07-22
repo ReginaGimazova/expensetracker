8bf842d6203f89edd8e9c717643b9dca56ead4e2
Regina-Gimazova <regina.gimazova@flatstack.com>
2019-07-22 17:08:19 +0300

    fix lint

--- src/pages/mainPage/index.jsx
+++ src/pages/mainPage/index.jsx
@@ -1,14 +1,11 @@
-import React from "react";
-import MainTemplate from "../../templates/MainTemplate";
-import "../../stylesheets/main.css";
-
+import React from 'react';
+import MainTemplate from '../../templates/MainTemplate';
+import '../../stylesheets/main.css';
 
 const MainPage = () => (
-    <MainTemplate>
-        <div className="container mainPageContainer">
-        
-        </div>
-    </MainTemplate>
+  <MainTemplate>
+    <div className="container mainPageContainer" />
+  </MainTemplate>
 );
 
 export default MainPage;
